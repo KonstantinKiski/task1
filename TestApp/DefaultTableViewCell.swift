7cd0c1caf2f74e31dcbcd9cc768a2ef9eb078388
Konstantin Kiski <kostyankiski@gmail.com>
2020-09-11 16:10:08 +0400

    finish

---
+++ TestApp/DefaultTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultTableViewCell.swift
+//  TestApp
+//
+//  Created by Константин Киски on 11.09.2020.
+//  Copyright © 2020 Константин Киски. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DefaultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var indexImage: UIImageView!
+    
+}
--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    let imageCache = NSCache<AnyObject, AnyObject>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as! DefaultTableViewCell
+        
+        if let url = URL(string: "https://placehold.it/375x150?text=\(indexPath.row+1)") {
+            downloadImage(imageUrl: url, cell: cell)
+        }
+        
+        return cell
+    }
+    
+    
+    func downloadImage(imageUrl: URL, cell: DefaultTableViewCell) {
+        
+        cell.indexImage?.image = nil
+        
+        if let imageFromCache = imageCache.object(forKey: imageUrl as AnyObject) as? UIImage {
+            cell.indexImage?.image = imageFromCache
+            return
+        }
+        
+        
+        URLSession.shared.dataTask(with: imageUrl, completionHandler: { (data, response, error) in
+            guard error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                if let unwrappedData = data, let imageToCache = UIImage(data: unwrappedData) {
+                    cell.indexImage?.image = imageToCache
+                    self.imageCache.setObject(imageToCache, forKey: imageUrl as AnyObject)
+                }
+            })
+        }).resume()
+    }
 }
+
 
